ba4522dbb9eb9923d7d299569fb493e6f4bf8662
Chatter-Boo <psakhil211@gmail.com>
2021-11-24 19:29:56 +0530

    second push

--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_payment_app/component/colors.dart';
+import 'package:flutter_payment_app/widgets/text_size.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -89,48 +90,145 @@ class _MyHomePageState extends State<MyHomePage> {
 
   _listBills() {
     return Positioned(
-      top: 320,
-      child: Container(
-        height: 130,
-        width: MediaQuery.of(context).size.width - 20,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.only(
-            topRight: Radius.circular(30),
-            bottomRight: Radius.circular(30),
-          ),
-          boxShadow: [
-            BoxShadow(
-                color: Color(0xFFd8dbe0),
-                offset: Offset(1, 1),
-                blurRadius: 20.0,
-                spreadRadius: 10),
-          ],
-        ),
-        child: Container(
-          margin: const EdgeInsets.only(top: 10, left: 18),
-          child: Row(
-            children: [
-              Column(
-                children: [
-                  Row(
-                    children: [
-                      Container(
-                        height: 60,
-                        width: 60,
-                        decoration: BoxDecoration(
-                            image: DecorationImage(
-                                fit: BoxFit.cover,
-                                image: AssetImage("images/background.png"))),
-                      ),
-                    ],
-                  )
+        top: 331,
+        child: Column(
+          children: [
+            Container(
+              height: 130,
+              width: MediaQuery.of(context).size.width - 20,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(30),
+                  bottomRight: Radius.circular(30),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                      color: Color(0xFFd8dbe0),
+                      offset: Offset(1, 1),
+                      blurRadius: 20.0,
+                      spreadRadius: 10),
                 ],
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+              ),
+              child: Container(
+                margin: const EdgeInsets.only(top: 10, left: 18),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Container(
+                              height: 60,
+                              width: 60,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  border: Border.all(
+                                    width: 3,
+                                    color: Colors.grey,
+                                  ),
+                                  image: DecorationImage(
+                                      fit: BoxFit.cover,
+                                      image: AssetImage("images/brand1.png"))),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Power',
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      color: AppColor.mainColor,
+                                      fontWeight: FontWeight.w700),
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  'ID: 232342',
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      color: AppColor.idColor,
+                                      fontWeight: FontWeight.w700),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        SizedText(
+                          text: "Pay on 20th",
+                          color: AppColor.green,
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              width: 80,
+                              height: 30,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(30),
+                                  color: AppColor.selectBackground),
+                              child: Center(
+                                child: Text(
+                                  'select',
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      color: AppColor.selectColor),
+                                ),
+                              ),
+                            ),
+                            Expanded(child: Container()),
+                            Text(
+                              '\$12.34',
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w800,
+                                  color: AppColor.selectColor),
+                            ),
+                            Text(
+                              'Due in 3 days',
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w700,
+                                  color: AppColor.idColor),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Container(
+                          height: 35,
+                          width: 5,
+                          decoration: BoxDecoration(
+                              color: AppColor.halfOval,
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(30),
+                                bottomLeft: Radius.circular(30),
+                              )),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
